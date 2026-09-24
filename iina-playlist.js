89987f855c69fc4cfd7e9c1a247fb873b434038e
Rogengo <31537482+Rogengo@users.noreply.github.com>
2026-09-24 20:17:37 +0300

    Update iina-playlist.js

--- iina-playlist.js
+++ iina-playlist.js
@@ -36,28 +36,11 @@
         // чтобы не подцепить JS-поведение фильтра.
         var html =
             '<div class="simple-button simple-button--filter selector iina-btn">' +
-                '<span>m3u</span>' +
-                '<div class="iina-icon">' +
-                    '<svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor">' +
-                        '<path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 ' +
-                              '10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 ' +
-                              '10.9 13 11.5 13 13h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26' +
-                              'c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 ' +
-                              '1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z"/>' +
-                    '</svg>' +
+                '<span>m3u Download</span>' +
                 '</div>' +
             '</div>';
 
         var $btn = $(html);
-
-        // Иконка чуть меньше текста и выровнена по центру
-        $btn.find('.iina-icon').css({
-            display: 'inline-flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            marginLeft: '.4em',
-            opacity: '.85'
-        });
 
         $btn.on('hover:enter click', function (e) {
             e.preventDefault();
